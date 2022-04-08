9cdc6573a4999ed20d70bdedfeaf0b2d3a046ab8
DiegoNicolas-utn <diego.ciccarelli@alumnos.frgp.utn.edu.ar>
2022-04-08 12:27:23 -0300

    Creacion subclase producto refrigerado

--- Tp2_G3_labIV/src/ejercicio_2/MainEjercicio2.java
+++ Tp2_G3_labIV/src/ejercicio_2/MainEjercicio2.java
@@ -6,7 +6,9 @@ public class MainEjercicio2 {
 
 		Pfrescos pf = new Pfrescos();
 		System.out.println(pf.toString());
-		
+		Prefrigerados pr = new Prefrigerados();
+		pr.setOrganismoSupervicionAlimentario(10);
+		System.out.println(pr.toString());
 		
 
 	}
---
+++ Tp2_G3_labIV/src/ejercicio_2/Prefrigerados.java
@@ -0,0 +1,55 @@
+package ejercicio_2;
+
+import java.util.Objects;
+
+public class Prefrigerados extends Producto{
+
+	private int OrganismoSupervicionAlimentario;
+	
+	Prefrigerados (){
+		super();
+		this.OrganismoSupervicionAlimentario = 0000;
+	}
+	
+	Prefrigerados(String FechaDeCaducidad,int NumeroDeLote, int OrganismoSupervicion){
+		super(FechaDeCaducidad,NumeroDeLote);
+		this.OrganismoSupervicionAlimentario = OrganismoSupervicion;
+		
+		}
+
+	public int getOrganismoSupervicionAlimentario() {
+		return OrganismoSupervicionAlimentario;
+	}
+
+	public void setOrganismoSupervicionAlimentario(int organismoSupervicionAlimentario) {
+		OrganismoSupervicionAlimentario = organismoSupervicionAlimentario;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + OrganismoSupervicionAlimentario;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Prefrigerados other = (Prefrigerados) obj;
+		if (OrganismoSupervicionAlimentario != other.OrganismoSupervicionAlimentario)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "\nProducto de tipo: Prefrigerados \nCódigo del Organismo de Supervicion Alimentario: " + OrganismoSupervicionAlimentario;
+	}
+	
+}
